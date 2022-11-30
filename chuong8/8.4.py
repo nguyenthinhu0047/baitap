8bfcb140609b3c484e4f83ac40acd136f71f7269
nguyenthinhu0047 <ntnhu.dhkl16a2hn@sv.uneti.edu.vn>
2022-11-30 11:46:43 +0700

    moi

--- chuong8/8.4.py
+++ chuong8/8.4.py
@@ -5,3 +5,13 @@ def dientichtg(a, b, c):
 
 
 print('diện tích tam giác:', dientichtg(8, 5, 6))
+
+
+import math
+print('nhập số đo các cạnh tam giácL:')
+a=eval(input('a = '))
+b=eval(input('b = '))
+c=eval(input('c = '))
+p=(a + b + c)/2
+s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+print('diện tích tam giác là :',s)
