dcd95ead4709a41d1d1bfe1157c353ce9683fc7c
nguyenthinhu0047 <ntnhu.dhkl16a2hn@sv.uneti.edu.vn>
2022-12-26 08:57:37 +0700

    n

--- 02DEMO/libs/v.py
+++ 02DEMO/libs/v.py
@@ -1,4 +1,6 @@
 # quan ly hoa don
+import os
+__path=""
 lsthodan =[]
 
 def mo_file_ho_dan(lsthodan):
@@ -26,13 +28,18 @@ print(lsthodan)
 def ds_ho_dan(lsthodan):
     print('{:12}{:12}{:18}{:18}'.format('ma_ho','ten_ho','so_tv','muc_thu_nhap'))
     for hd in lsthodan:
-     print('{:12}{:12}{:18}{:18}'.format(hd['ma_ho'],hd['ten_hd'],hd['so_tv'],hd['muc_thu_nhap']))
+     print('{:12}{:12}{:18}{:18}'.format(hd['ma_ho'],hd['ten_ho'],hd['so_tv'],hd['muc_thu_nhap']))
     return
 print(ds_ho_dan(lsthodan))
-def tinh_tien_tro_cap(so_tv):
-    if so_tv >=5:
+def tinh_tien_tro_cap(ho_ngheo,so_tv):
+    if (ho_ngheo and so_tv >=5):
                 return 1000000*so_tv
-    elif 3 <= so_tv < 5:
+    elif (ho_ngheo and 3 <= so_tv < 5):
                 return 800000*so_tv  
-    elif 1<= so_tv <3:
+    elif (ho_ngheo and 1<= so_tv <3):
                 return 500000*so_tv           
+print(tinh_tien_tro_cap(ho_ngheo,so_tv))
+def sap_xep_dan(lsthodan):
+    lsthodan.sort()
+    lsthodan.reverse()
+print(lsthodan)
--- b/n.py
+++ b/n.py
@@ -1,1 +1,8 @@
-bbb
+def tinh_tien_tro_cap(ho_ngheo,so_tv):
+    if (ho_ngheo or so_tv >=5):
+                return 1000000*so_tv
+    elif (ho_ngheo,3 <= so_tv < 5):
+                return 800000*so_tv  
+    elif (ho_ngheo ,1<= so_tv <3):
+                return 500000*so_tv           
+print(tinh_tien_tro_cap(ho_ngheo,so_tv))
