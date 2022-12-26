4057d5e1e0a03b4e9d174b79c64627deaca78d48
nguyenthinhu0047 <ntnhu.dhkl16a2hn@sv.uneti.edu.vn>
2022-12-26 18:21:55 +0700

    N

--- 02DEMO/libs/v.py
+++ 02DEMO/libs/v.py
@@ -11,6 +11,7 @@ def them_ho_dan(lsthodan):
      while True:
         ma_ho=input('Nhập mã hộ dân')
         ten_ho=input('tên hộ dân: ')
+       
         so_tv=int(input('số thành vien: '))
         muc_thu_nhap=float(input("mức thu nhập: "))
         hongheo=input('bạn là hộ nghèo hay không?(0 bạn là hộ nghèo:1 bạn không là hộ nghèo)')
@@ -21,19 +22,23 @@ def them_ho_dan(lsthodan):
             'so_tv':so_tv,'muc_thu_nhap':muc_thu_nhap,'hongheo':hongheo})
             tt=input('Bạn có muốn tiếp tục thêm ? (1:TT)')
             if tt!='1':
-                return 
-            break         
+                break
+        return    
+
+
+
+    
 print(them_ho_dan(lsthodan))
 print(lsthodan)
 def ds_ho_dan(lsthodan):
-    print('{:12}{:12}{:12}{:12}'.format('ma_ho','ten_ho','so_tv','muc_thu_nhap','hongheo'))
+    print('{:12}{:12}{:18}{:18}'.format('ma_ho','ten_ho','so_tv','muc_thu_nhap','hongheo'))
     for hd in lsthodan:
-     print('{:12}{:12}{:9}{:12}'.format(hd['ma_ho'],hd['ten_ho'],hd['so_tv'],hd['muc_thu_nhap'],hd['hongheo']))
+     print('{:12}{:12}{:18}{:18}'.format(hd['ma_ho'],hd['ten_ho'],hd['so_tv'],hd['muc_thu_nhap'],hd['hongheo']))
     return
 print(ds_ho_dan(lsthodan))
 
 def tinh_tien_tro_cap(so_tv):
-    
+   
     if 3 >= so_tv >=5:
                 return 1000000*so_tv
     elif ( 3 <= so_tv < 5):
